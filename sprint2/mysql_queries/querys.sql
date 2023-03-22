98da40767622178cde97c7391c262e5ea3635da4
Roberto Cortijo <developer.gsg@gmail.com>
2023-03-22 11:23:03 +0100

    correcciones mysql queries

--- sprint2/mysql_queries/querys.sql
+++ sprint2/mysql_queries/querys.sql
@@ -8,7 +8,7 @@ SELECT nombre FROM producto;
 SELECT nombre, precio FROM producto;
 
 #3.Lista todas las columnas de la tabla "producto".
-SELECT *FROM producto;
+SHOW COLUMNS FROM producto;
 
 #4.Lista el nombre de los "productos", el precio en euros y el precio en dólares estadounidenses (USD).
 SELECT nombre, precio AS precio_euros, (precio*1.09) AS precio_usd FROM producto;
@@ -23,7 +23,7 @@ SELECT UPPER(nombre) AS nombre, precio FROM producto;
 SELECT LOWER(nombre) AS nombre, precio FROM producto;
 
 #8.Lista el nombre de todos los fabricantes en una columna, y en otra columna obtenga en mayúsculas los dos primeros caracteres del nombre del fabricante.
-SELECT nombre, LEFT(nombre,2) AS caracteres FROM fabricante;
+SELECT nombre, UPPER(LEFT(nombre,2)) AS caracteres FROM fabricante;
 
 #9.Lista los nombres y precios de todos los productos de la tabla "producto", redondeando el valor del precio.
 SELECT nombre, ROUND(precio) AS precio FROM producto;
@@ -35,7 +35,7 @@ SELECT nombre, TRUNCATE(precio,0) AS precio_truncado FROM producto;
 SELECT `codigo_fabricante` FROM `producto`;
 
 #12.Lista el código de los fabricantes que tienen productos en la tabla "producto", eliminando los códigos que aparecen repetidos.
-SELECT `codigo_fabricante` FROM `producto` GROUP BY `codigo_fabricante`;
+SELECT DISTINCT(`codigo_fabricante`) FROM `producto`;
 
 #13.Lista los nombres de los fabricantes ordenados de forma ascendente.
 SELECT nombre FROM `fabricante` ORDER BY nombre ASC;
@@ -50,6 +50,7 @@ SELECT nombre FROM `producto` ORDER BY nombre ASC, `precio` DESC;
 SELECT * FROM `fabricante` LIMIT 5;
 
 #17.Devuelve una lista con 2 filas a partir de la cuarta fila de la tabla "fabricante". La cuarta fila también debe incluirse en la respuesta.
+SELECT *FROM fabricante LIMIT 3, 2;
 
 #18.Lista el nombre y precio del producto más barato. (Utiliza solo las cláusulas ORDER BY y LIMIT). NOTA: Aquí no podrías usar MIN(precio), necesitarías GROUP BY
 SELECT nombre, precio FROM producto ORDER BY precio ASC LIMIT 1;
@@ -112,10 +113,10 @@ SELECT a.* FROM producto a WHERE a.codigo_fabricante=(SELECT codigo FROM `fabric
 SELECT a.* FROM producto a WHERE a.precio = (SELECT MAX(precio) FROM producto WHERE codigo_fabricante = (SELECT codigo FROM `fabricante` WHERE nombre='Lenovo'));
 
 #38.Lista el nombre del producto más caro del fabricante Lenovo.
-SELECT a.nombre, MAX(a.`precio`) AS precio  FROM producto a INNER JOIN `fabricante` b ON a.`codigo_fabricante`=b.`codigo` WHERE b.`nombre`='Lenovo' GROUP BY b.`nombre`;
+SELECT a.nombre FROM producto a INNER JOIN fabricante f ON a.`codigo_fabricante`=f.`codigo` WHERE f.`nombre`='Lenovo' ORDER BY a.`precio` DESC LIMIT 1; 
 
 #39.Lista el nombre del producto más barato del fabricante Hewlett-Packard.
-SELECT a.nombre, MIN(a.`precio`) AS precio  FROM producto a INNER JOIN `fabricante` b ON a.`codigo_fabricante`=b.`codigo` WHERE b.`nombre`='Hewlett-Packard' GROUP BY b.`nombre`;
+SELECT a.nombre FROM producto a INNER JOIN fabricante f ON a.`codigo_fabricante`=f.`codigo` WHERE f.`nombre`='Hewlett-Packard' ORDER BY a.`precio` ASC LIMIT 1; 
 
 #40.Devuelve todos los productos de la base de datos que tienen un precio mayor o igual al producto más caro del fabricante Lenovo.
 SELECT *FROM producto WHERE precio >= (SELECT MAX(a.`precio`) AS precio  FROM producto a INNER JOIN `fabricante` b ON a.`codigo_fabricante`=b.`codigo` WHERE b.`nombre`='Lenovo' GROUP BY b.`nombre`);
@@ -127,13 +128,13 @@ SELECT *FROM producto a INNER JOIN fabricante b ON a.`codigo_fabricante` = b.cod
 # BD: universidad	#
 #########################
 #1.Devuelve un listado con el primer apellido, segundo apellido y el nombre de todos los alumnos/as. El listado deberá estar ordenado alfabéticamente de menor a mayor por el primer apellido, segundo apellido y nombre.
-SELECT a.apellido1 AS primer_apellido, a.apellido2 AS segundo_apellido, a.nombre FROM `persona` a INNER JOIN `alumno_se_matricula_asignatura` b ON a.`id`=b.`id_alumno` GROUP BY b.`id_alumno`;
+SELECT apellido1 AS primer_apellido, apellido2 AS segundo_apellido, nombre FROM `persona` WHERE tipo='alumno';
 
 #2.Averigua el nombre y los dos apellidos de los alumnos que no han dado de alta su número de teléfono en la base de datos.
-SELECT nombre, CONCAT(apellido1,' ',apellido2) AS apellidos FROM persona WHERE telefono = '' OR telefono IS NULL;
+SELECT nombre, CONCAT(apellido1,' ',apellido2) AS apellidos FROM persona WHERE telefono = '' OR telefono IS NULL AND tipo='alumno';
 
 #3.Devuelve el listado de los alumnos/as que nacieron en 1999.
-SELECT nombre, CONCAT(apellido1,' ',apellido2) AS apellidos FROM persona WHERE YEAR(`fecha_nacimiento`)='1999';
+SELECT nombre, CONCAT(apellido1,' ',apellido2) AS apellidos FROM persona WHERE YEAR(`fecha_nacimiento`)='1999' AND tipo='alumno';;
 
 #4.Devuelve el listado de profesores/as que no han dado de alta su número de teléfono en la base de datos y además su NIF termina en K.
 SELECT nombre, CONCAT(apellido1,' ',apellido2) AS apellidos  FROM persona a INNER JOIN profesor b ON a.`id`=b.`id_profesor` WHERE a.telefono = '' OR a.telefono IS NULL AND RIGHT(a.`nif`,1)='K';
@@ -145,32 +146,32 @@ SELECT * FROM `asignatura` WHERE cuatrimestre='1' AND curso='3' AND id_grado='7'
 SELECT p.`apellido1`, p.`apellido2`, p.`nombre`, b.`nombre` AS departamento FROM profesor a INNER JOIN persona p ON a.`id_profesor`=p.`id` INNER JOIN departamento b ON a.`id_departamento`=b.`id` ORDER BY p.`apellido1`, p.`apellido2`, p.nombre;
 
 #7.Devuelve un listado con el nombre de las asignaturas, año de inicio y año de fin del curso escolar del alumno/a con NIF 26902806M.
-SELECT a.`nombre`,b.`anyo_inicio`,b.`anyo_fin` FROM `asignatura` a INNER JOIN `curso_escolar` b ON a.`curso`=b.`id` INNER JOIN `alumno_se_matricula_asignatura` c ON c.`id_curso_escolar`=b.`id` INNER JOIN `persona` d ON d.`id`=c.`id_alumno` WHERE d.`nif`='26902806M' GROUP BY c.`id_alumno`;
+SELECT a.nombre,c.anyo_inicio,c.anyo_fin,p.nif FROM alumno_se_matricula_asignatura am INNER JOIN persona p ON p.id = am.id_alumno INNER JOIN curso_escolar c ON p.id = c.id INNER JOIN asignatura a ON am.id_asignatura = a.id AND p.nif = '26902806M';
 
 #8.Devuelve un listado con el nombre de todos los departamentos que tienen profesores/as que imparten alguna asignatura en el Grado en Ingeniería Informática (Plan 2015).
 SELECT c.nombre FROM profesor a INNER JOIN `asignatura` b ON a.`id_profesor`=b.`id_profesor` INNER JOIN `departamento` c ON c.`id`=a.`id_departamento` WHERE b.`id_grado`='4' GROUP BY c.`id`;
 
 #9.Devuelve un listado con todos los alumnos/as que se han matriculado en alguna asignatura durante el curso escolar 2018/2019.
-SELECT a.* FROM `persona` a INNER JOIN `alumno_se_matricula_asignatura` b ON a.`id`=b.`id_alumno` WHERE b.`id_curso_escolar`='5' GROUP BY a.`id`;
+SELECT a.nombre,a.apellido1,a.apellido2,asig.nombre FROM persona a INNER JOIN alumno_se_matricula_asignatura am ON a.id = am.id_alumno INNER JOIN curso_escolar ce ON am.id_curso_escolar = ce.id INNER JOIN asignatura asig ON am.id_asignatura = asig.id WHERE ce.anyo_inicio = 2018 AND ce.anyo_fin = 2019;
 
 #Resuelve las 6 siguientes consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN.
 #1.Devuelve un listado con los nombres de todos los profesores/as y los departamentos que tienen vinculados/as. El listado también debe mostrar aquellos profesores/as que no tienen ningún departamento asociado. El listado debe devolver cuatro columnas, nombre del departamento, primer apellido, segundo apellido y nombre del profesor/a. El resultado estará ordenado alfabéticamente de menor a mayor por el nombre del departamento, apellidos y nombre.
-SELECT c.`nombre` AS nombre_departamento,a.`apellido1` AS primer_apellido,a.`apellido2` AS segundo_apellido,a.`nombre` FROM persona a INNER JOIN profesor b ON a.`id`=b.`id_profesor` LEFT JOIN departamento c ON c.`id`=b.`id_departamento` WHERE b.`id_departamento` IS NULL;
+SELECT d.nombre AS 'Departamento',per.apellido1 AS 'Primer Apellido', per.apellido2 AS 'Segundo Apellido', per.nombre AS 'Nombre Profesor',p.id_departamento FROM departamento d RIGHT JOIN profesor p ON d.id = p.id_departamento INNER JOIN persona per ON per.`id`=p.`id_profesor` ORDER BY d.nombre, per.apellido1, per.apellido2, per.nombre;
 
 #2.Devuelve un listado con los profesores/as que no están asociados a un departamento.
-SELECT b.`id_profesor`,a.`nombre`,a.`apellido1`,a.`apellido2` FROM persona a INNER JOIN profesor b ON a.`id`=b.`id_profesor` LEFT JOIN departamento c ON c.`id`=b.`id_departamento` WHERE b.`id_departamento` IS NULL;
+SELECT per.apellido1 AS 'Apellido1',per.apellido2 AS 'Apellido2',per.nombre AS 'Nombre',d.nombre 'Departamento' FROM profesor p INNER JOIN persona per ON per.`id`=p.`id_profesor` LEFT JOIN departamento d ON p.id_departamento = d.id WHERE p.id_departamento IS NULL ORDER BY d.nombre, per.apellido1, per.apellido2, per.nombre;
 
 #3.Devuelve un listado con los departamentos que no tienen profesores asociados.
 SELECT *FROM departamento a LEFT JOIN profesor b ON a.`id`=b.`id_departamento` WHERE b.`id_departamento` IS NULL;
 
 #4.Devuelve un listado con los profesores/as que no imparten ninguna asignatura.
-SELECT pe.`apellido1`,pe.`apellido2`,pe.`nombre` FROM profesor a INNER JOIN persona pe ON pe.`id`= a.`id_profesor` AND pe.`tipo`='profesor' LEFT JOIN asignatura b ON a.`id_profesor`=b.`id_profesor` WHERE b.`id_profesor` IS NULL;
+SELECT p.`apellido1`,p.`apellido2`,p.`nombre` FROM profesor prof INNER JOIN persona p ON p.`id`=prof.`id_profesor` LEFT JOIN asignatura asig ON prof.`id_profesor`=asig.id_profesor WHERE prof.`id_profesor` IS NULL;
 
 #5.Devuelve un listado con las asignaturas que no tienen un profesor/a asignado.
 SELECT *FROM asignatura WHERE id_profesor IS NULL;
 
 #6.Devuelve un listado con todos los departamentos que no han impartido asignaturas en ningún curso escolar.
-SELECT a.`id`,a.nombre FROM departamento a INNER JOIN profesor b ON b.`id_departamento`=a.`id` LEFT JOIN asignatura c ON c.`id_profesor`=b.`id_profesor` WHERE c.`curso` IS NULL GROUP BY a.`nombre`;
+SELECT d.nombre AS 'Departamento' FROM departamento d INNER JOIN profesor p ON d.id = p.id_departamento INNER JOIN asignatura a ON p.`id_profesor` = a.id_profesor LEFT JOIN curso_escolar c ON a.curso = c.id WHERE c.id IS NULL;
 
 #Consultas resumen:
 #1.Devuelve el número total de alumnos existentes.
@@ -195,7 +196,7 @@ SELECT a.nombre, COUNT(b.`id`) AS nro_asignaturas FROM grado a LEFT JOIN asignat
 SELECT b.nombre,a.tipo AS tipo_asignatura,SUM(a.`creditos`) AS creditos FROM asignatura a INNER JOIN grado b ON a.`id_grado`=b.`id` GROUP BY b.`nombre`,a.tipo;
 
 #8.Devuelve un listado que muestre cuántos/as alumnos/as se han matriculado de alguna asignatura en cada uno de los cursos escolares. El resultado deberá mostrar dos columnas, una columna con el año de inicio del curso escolar y otra con el número de alumnos/as matriculados/as.
-SELECT a.`anyo_inicio`, COUNT(b.`id_curso_escolar`) AS cantidad_matriculados FROM `curso_escolar` a LEFT JOIN alumno_se_matricula_asignatura b ON b.`id_curso_escolar`=a.`id` GROUP BY a.`id`;
+SELECT ce.anyo_inicio AS anio_escolar, COUNT(alumno_se_matricula_asignatura.id_alumno) AS nro_matriculados FROM curso_escolar ce LEFT JOIN alumno_se_matricula_asignatura ON  ce.id = alumno_se_matricula_asignatura.id_curso_escolar GROUP BY ce.anyo_inicio ORDER BY COUNT(alumno_se_matricula_asignatura.id_alumno);
 
 #9.Devuelve un listado con el número de asignaturas que imparte cada profesor/a. El listado debe tener en cuenta a aquellos profesores/as que no imparten ninguna asignatura. El resultado mostrará cinco columnas: id, nombre, primer apellido, segundo apellido y número de asignaturas. El resultado estará ordenado de mayor a menor por el número de asignaturas.
 SELECT b.`id_profesor`,a.`nombre`, a.`apellido1`, a.`apellido2`, COUNT(c.`id`) AS nro_asignaturas  FROM persona a INNER JOIN profesor b ON a.`id`=b.`id_profesor` LEFT JOIN asignatura c ON c.`id_profesor`=b.`id_profesor` GROUP BY b.`id_profesor` ORDER BY nro_asignaturas DESC;
